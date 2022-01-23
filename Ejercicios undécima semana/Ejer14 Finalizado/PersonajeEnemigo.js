2927df31ca8edf94a74fe9e87e6703d6bfb6a880
abranosrodriguez <92040609+abranosrodriguez@users.noreply.github.com>
2022-01-23 22:30:36 +0100

    Delete Ejercicios undécima semana directory

--- Ejercicios undécima semana/Ejer14 Finalizado/PersonajeEnemigo.js
+++
@@ -1,9 +0,0 @@
-class PersonajeEnemigo {
-    constructor(nombre, nivel, vida, ataque, experiencia) {
-        this.nombre = nombre;
-        this.nivel = nivel;
-        this.vida = vida;
-        this.ataque = ataque;
-        this.experiencia = experiencia;
-    }
-}
